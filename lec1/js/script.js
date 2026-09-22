679af7af944cc9220312eac05e7b9a16e34b5984
abdallahubeid <abdullahubeid@icloud.com>
2026-09-22 23:56:22 +0300

    introduction and variables

---
+++ lec1/js/script.js
@@ -0,0 +1,53 @@
+// function printMySalary() {
+//     window.alert('1000$');
+// }
+
+// var age = 10;
+
+// console.log(age);
+
+// var age = 10;
+
+// if (true) {
+//     var age = 20;
+// }
+
+// console.log(age);
+
+
+// let age = 10;
+
+// console.log(age);
+
+// let age = 10;
+// let age = 20;
+// if (true) {
+//     let age = 20;
+// }
+// console.log(age);
+
+// const age = 10;
+// // const age = 100;
+// if (true) {
+//     let age = 20;
+// }
+// console.log(age);
+
+//object
+// const user = {
+//     name: 'Ra3d',
+//     age: 20,
+// };
+
+// user.name = 'Ra3d ra3d';
+
+// const user = {
+//     name: 'Ra3d',
+//     age: 20,
+// };
+
+// user = {
+//     name: 'Ra33d',
+//     age: 30,
+// };;
+// console.log(user);
