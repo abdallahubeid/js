46e6e842bc8c86a6657ccc0bd9f55e6b130e487b
abdallahubeid <abdullahubeid@icloud.com>
2026-09-26 21:12:28 +0300

    Object and function

--- lec 4/js/script.js
+++ lec 4/js/script.js
@@ -1,17 +1,18 @@
-let x = 2;
-let y = 5;
+// let x = 2;
+// let y = 5;
 
 // console.log(sum(x, y));
 
-function sum(x, y) {
-    console.log(x + y);
-}
+// function sum(x, y) {
+//     console.log(x + y);
+// }
 
-// هيك نفذ الدالة قبل الحدث 
+// هيك نفذ الدالة قبل الحدث
 // document.addEventListener('click', sum(x, y));
 
 // هيك صح نفذ بعد  الحدث
 
-document.addEventListener('click', function () {
-    sum(x, y);
-});
+// document.addEventListener('click', function () {
+//     sum(x, y);
+// });
+
